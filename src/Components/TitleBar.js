8b32645fc11f1094b60683acf64bbf55cbbe363d
Your Name <you@example.com>
2020-10-21 17:15:28 +0200

    src fix

--- src/Components/TitleBar.js
+++ src/Components/TitleBar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './TitleBar.css';
 
-
 const TitleBar = () => {
     return (
         <div className="titleBar">
-            <h1>Find Your Fluffy Friend</h1>
-            <img className="titleImg" src="../images/paw-sil.png" alt=""/>
+            <h1>Find Your <span>Fluffy</span> Friend</h1>
+            <img className="titleImg" src={require('../images/paw-sil.png')} alt=""/>
         </div>
     );
 }
