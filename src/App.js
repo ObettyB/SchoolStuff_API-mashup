67911b82914a4b5147018ce71d4ab281ae772e7e
Your Name <you@example.com>
2020-10-21 16:27:56 +0200

    header + background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import TitleBar from "./Components/TitleBar"
 
 const App = () => {
   return (
     <div className="App">
+      <TitleBar />
+
+      <form>
+        <select className="breedSelection" id="breeds"> 
+          <option>TestOption1</option>
+          <option>TestOption2</option>
+          <option>TestOption3</option>
+          <option>TestOption4</option>
+        </select>
+      </form>
+
       <h1>Hello React</h1>
-      <h2>Hello Node</h2>
+      <h2>Hello you!</h2>
+
+      
     </div>
   );
 }
---
+++ src/Components/TitleBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TitleBar.css';
+
+
+const TitleBar = () => {
+    return (
+        <div className="titleBar">
+            <h1>Find Your Fluffy Friend</h1>
+            <img className="titleImg" src="../images/paw-sil.png" alt=""/>
+        </div>
+    );
+}
+
+export default TitleBar;
